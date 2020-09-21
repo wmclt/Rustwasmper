83e6ca7c376be14262353931607153d2c867df64
benbertrands <b.public.griffin@gmail.com>
2020-09-21 12:59:49 +0200

    trying right click flag

--- src/pages/board.rs
+++ src/pages/board.rs
@@ -1,14 +1,13 @@
 use rand::Rng;
 use std::collections::HashMap;
 use yew::{html, Component, ComponentLink, Html, ShouldRender};
-
 const SIZE: u32 = 20;
 const NR_OF_BOMBS: u32 = 50;
 
 #[derive(Debug, Copy, Clone)]
 pub enum Tile {
-    Bomb(bool),
-    NotBomb(u8, bool),
+    Bomb(bool, bool),
+    NotBomb(u8, bool, bool),
 }
 
 pub struct Board {
@@ -21,7 +20,7 @@ fn generate_board() -> HashMap<(u32, u32), Tile> {
     let mut tiles: HashMap<(u32, u32), Tile> = HashMap::with_capacity((SIZE * SIZE) as usize);
     for row in 0..SIZE {
         for column in 0..SIZE {
-            tiles.insert((column, row), Tile::NotBomb(0, false));
+            tiles.insert((column, row), Tile::NotBomb(0, false, false));
         }
     }
 
@@ -30,19 +29,19 @@ fn generate_board() -> HashMap<(u32, u32), Tile> {
         loop {
             location = random_bomb_location();
             match tiles[&location] {
-                Tile::Bomb(_) => continue,
+                Tile::Bomb(_, _) => continue,
                 _ => {
                     break;
                 }
             }
         }
 
-        tiles.insert(location, Tile::Bomb(false));
+        tiles.insert(location, Tile::Bomb(false, false));
 
         let increment_neighs = |coord: &(u32, u32), tiles: &mut HashMap<(u32, u32), Tile>| {
             let tile = tiles.get(coord).unwrap();
             match tile {
-                Tile::NotBomb(nr, _) => Tile::NotBomb(nr + 1, false),
+                Tile::NotBomb(nr, _, _) => Tile::NotBomb(nr + 1, false, false),
                 _ => *tile,
             }
         };
@@ -74,6 +73,7 @@ fn random_bomb_location() -> (u32, u32) {
 
 pub enum Msg {
     Select(u32, u32),
+    RightClick(u32, u32)
 }
 
 impl Component for Board {
@@ -95,6 +95,13 @@ impl Component for Board {
         match msg {
             Msg::Select(col, row) => {
                 self.click(col, row);
+            },
+            Msg::RightClick(col, row) => {
+                match self.tiles[&(col, row)] {
+                    Tile::Bomb(false, false) => self.tiles.insert((col, row), Tile::Bomb(false, true)),
+                    Tile::NotBomb(nr, false, false) => self.tiles.insert((col, row), Tile::NotBomb(nr, false, true)),
+                    _ => { }
+                }
             }
         }
         true
@@ -105,7 +112,7 @@ impl Component for Board {
     }
 
     fn view(&self) -> Html {
-        html! {
+        html! {      
             <table>
                 { (0..SIZE).map(|row| self.view_row(row)).collect::<Html>() }
             </table>
@@ -121,17 +128,17 @@ impl Board {
     fn click(&mut self, col: u32, row: u32) {
 
             match self.tiles[&(col, row)] {
-                Tile::Bomb(_) => {
-                    self.tiles.insert((col, row), Tile::Bomb(true));
+                Tile::Bomb(_, false) => {
+                    self.tiles.insert((col, row), Tile::Bomb(true, false));
                     self.game_over = true;
                 }
-                Tile::NotBomb(0, false) => {
-                    self.tiles.insert((col, row), Tile::NotBomb(0, true));
+                Tile::NotBomb(0, false, false) => {
+                    self.tiles.insert((col, row), Tile::NotBomb(0, true, false));
 
                     for col_neigh in left(col)..=right(col) {
                         for row_neigh in above(row)..=below(row) {
                             match self.tiles[&(col_neigh, row_neigh)] {
-                                Tile::NotBomb(_, false) => {
+                                Tile::NotBomb(_, false, false) => {
                                     self.click(col_neigh, row_neigh)
                                 },
                                 _ => {}
@@ -139,15 +146,17 @@ impl Board {
                         }
                     }
                 }
-                Tile::NotBomb(nr, _) => {
-                    self.tiles.insert((col, row), Tile::NotBomb(nr, true));
-                }
+                Tile::NotBomb(nr, false, false) => {
+                    self.tiles.insert((col, row), Tile::NotBomb(nr, true, false));
+                },
+                _ => {}
             };
     }
     fn view_square(&self, row: u32, column: u32) -> Html {
         html! {
             <td class=self.square_class((column, row))
-                onclick=self.link.callback(move |_| Msg::Select(column, row))>
+                onclick=self.link.callback(move |_| Msg::Select(column, row))
+                oncontextmenu=self.link.callback(move |_| Msg::RightClick(column, row))>
                 <div> { self.get_tile_nr(column, row) } </div>
             </td>
         }
@@ -166,22 +175,24 @@ impl Board {
     fn square_class(&self, tile: (u32, u32)) -> &'static str {
         if self.game_over {
             match self.tiles[&tile] {
-                Tile::Bomb(_) => {
+                Tile::Bomb(_, _) => {
                     return "bomb";
                 }
                 _ => {}
             }
         }
         match self.tiles[&tile] {
-            Tile::NotBomb(_, true) => "touched_tile",
+            Tile::NotBomb(_, true, false) => "touched_tile",
+            Tile::NotBomb(_, false, true) => "flag",
+            Tile::Bomb(_, true) => "flag",
             _ => "untouched_tile",
         }
     }
 
     fn get_tile_nr(&self, col: u32, row: u32) -> String {
         match self.tiles[&(col, row)] {
-            Tile::NotBomb(0, true) => "".to_string(),
-            Tile::NotBomb(nr, true) => format!("{}", nr),
+            Tile::NotBomb(0, true, _) => "".to_string(),
+            Tile::NotBomb(nr, true, _) => format!("{}", nr),
             _ => "".to_string(),
         }
     }
